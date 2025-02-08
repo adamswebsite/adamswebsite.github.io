0f455071513782dfc9c3d2e29830cdc849412593
uxffm <35499917+uxffm@users.noreply.github.com>
2025-02-08 18:35:39 +0100

    First upload!

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+
+import vue from '@astrojs/vue';
+
+import svelte from '@astrojs/svelte';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react(), vue(), svelte()]
+});
---
+++ src/script.js
@@ -0,0 +1,1 @@
+console.log('Welcome, browser2 console!')
